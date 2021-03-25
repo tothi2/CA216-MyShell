724f3d710c297bb2aa65fac421be4c94cf7f8ca2
Imrich Toth <imrich.toth2@mail.dcu.ie>
2021-03-25 21:25:40 +0000

    Added utility.h

---
+++ src/utility.h
@@ -0,0 +1,42 @@
+#define MAX_BUFFER 1024                        // max line buffer
+#define MAX_ARGS 64                            // max # args
+#define SEPARATORS " \t\n"                     // token sparators
+
+int myshell(char *args[]);
+
+
+// personalized shell loading screen //
+
+void init()
+{
+    printf(" *  *  *  *  *  *  *  *  *  *  *  *  *  *\n");
+    printf("* * * * * * * * * * * * * * * * * * * * * *\n");
+    printf(" *  *  *  *  *  *  *  *  *  *  *  *  *  *\n");
+    printf("* * * * * * * * * * * * * * * * * * * * * *\n");
+    printf("*  *  *  Welcome to Imrichs Shell *  *  *\n");
+    printf("* * * * * * * * * * * * * * * * * * * * * *\n");
+    printf(" *  *  *  *  *  *  *  *  *  *  *  *  *  *\n");
+    printf("* * * * * * * * * * * * * * * * * * * * * *\n");
+    printf(" *  *  *  *  *  *  *  *  *  *  *  *  *  *\n");
+    sleep(4);
+
+}
+
+
+// ths will be used to execute internal commands //
+int execute_command(char *args[])
+{
+    char * commands[] = { "clr", "quit", "dir", "environ", "echo", "cd"}; // list of commands written in commands.c
+    int (*functions[]) (char **) = { &clr, &quit, &dir, &enviroment, &echo, &change_dir};
+    
+    // the input must match the function name in order for the command to be executed
+    for (int i = 0; commands[i]; i++)
+    {
+
+        if (!strcmp(args[0], commands[i]))
+        {
+          return (*functions[i])(args);
+        }
+    }
+
+}
