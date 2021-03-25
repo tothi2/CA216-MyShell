11af181ed549239dcdaf876815c05315ae9d5998
Imrich Toth <imrich.toth2@mail.dcu.ie>
2021-03-25 16:46:16 +0000

    Update myshell.c

--- src/myshell.c
+++ src/myshell.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include 'commands.c'
 
 #define MAX_BUFFER 1024                        // max line buffer
 #define MAX_ARGS 64                            // max # args
