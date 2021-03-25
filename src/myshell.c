6797db5e1798c93afd89594c38ad49ae254bfbb1
Imrich Toth <imrich.toth2@mail.dcu.ie>
2021-03-25 16:05:09 +0000

    Update myshell.c

--- src/myshell.c
+++ src/myshell.c
@@ -14,26 +14,22 @@ int main (int argc, char ** argv)
     char buf[MAX_BUFFER];                      // line buffer
     char * args[MAX_ARGS];                     // pointers to arg strings
     char ** arg;                               // working pointer thru args
-char * prompt = "==>" ;                    // shell prompt
+    char * prompt = "==>" ;                    // shell prompt
 
 /* keep reading input until "quit" command or eof of redirected input */
 
     while (!feof(stdin))
     { 
-
         /* get command line from input */
-
         fputs (prompt, stdout);                // write prompt
         if (fgets (buf, MAX_BUFFER, stdin )) 
         { // read a line
             /* tokenize the input into args array */
-            
             arg = args;
             *arg++ = strtok(buf,SEPARATORS);   // tokenize input
             while ((*arg++ = strtok(NULL,SEPARATORS)));
             // last entry will be NULL if (args[0]) 
             {                     // if there's anything there
-
             /* check for internal/external command */
             if (!strcmp(args[0],"clear")) 
             { // "clear" command
