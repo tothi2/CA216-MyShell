6e238462115cd4168331e49b0885119a7a93c19b
Imrich Toth <imrich.toth2@mail.dcu.ie>
2021-03-25 18:16:11 +0000

    Update commands.c

--- src/commands.c
+++ src/commands.c
@@ -7,10 +7,10 @@ int clr()
     command[0] = "clear";
     command[1] = NULL;
 
-    pid_t pid = fork();
-    if (pid == 0)
+    pid_t pid = fork(); // this fork() command returns twice, if the process fails it returns -1 //
+    if (pid == 0) // if the fork process succeeds then it returns a 0 to the child, therefore I will be using the fork method for checking if a process has failed or not. //
     {
-        execvp("clear", command);
+        execvp("clear", command); // I use execvp here as the child process does not need to run the same program as the parent. //
     }
     wait(NULL);
 }
